e482e0bf1dc6e1f0721da6ae421f315b292ee555
Jupri R <juprir@gmail.com>
2023-03-07 02:28:53 +0700

    wip

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return redirect()->route('dashboard');
-});
-
-Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 

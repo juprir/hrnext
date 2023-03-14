52bb421a3e2326189a3c55d4f1478aab63f3a9b0
Jupri R <juprir@gmail.com>
2023-03-14 11:46:23 +0700

    wip

---
+++ config/esign.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'host' => env('ESIGN_HOST', 'https://esign-dev.layanan.go.id'),
+    'client_id' => env('ESIGN_CLIENT_ID', ''),
+    'client_secret' => env('ESIGN_CLIENT_SECRET', ''),
+];
